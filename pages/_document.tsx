0449ace718c7f04e6266a64a2843a27ba2cab034
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-02-11 15:05:48 +0300

    add document component

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, {DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript} from "next/document";
+
+class MyDocument extends Document {
+
+    static async getInitialProps(context: DocumentContext): Promise<DocumentInitialProps> {
+        const initialProps = await Document.getInitialProps(context);
+
+        return {...initialProps};
+    }
+
+    render(): JSX.Element {
+        return (
+            <Html lang={'ru'}>
+                <Head><title>My next App</title></Head>
+                <body>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
