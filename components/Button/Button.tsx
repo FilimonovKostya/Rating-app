4f597d845ded19b5310bcfa031f04a1e7ce6239b
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-02-14 10:01:28 +0300

    Extend Button props

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,12 +2,14 @@ import {ButtonProps} from "./Button.props";
 import style from './Button.module.css';
 import cn from 'classnames';
 
-export const Button = ({appearance, children}: ButtonProps): JSX.Element => {
+export const Button = ({appearance, children, className, ...props}: ButtonProps): JSX.Element => {
     return (
-        <button className={cn(style.button, {
+        <button className={cn(style.button, className, {
             [style.primary]: appearance === 'primary',
             [style.ghost]: appearance === 'ghost'
-        })}>
+        })}
+                {...props}
+        >
             {children}
         </button>
     );
