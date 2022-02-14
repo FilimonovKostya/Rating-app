7b24c2aca208ab1204152c2cb829d7d28dbbe1dd
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-02-14 10:47:21 +0300

    was added svg icon and set up, added lib for svg

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,8 +1,9 @@
 import {ButtonProps} from "./Button.props";
 import style from './Button.module.css';
 import cn from 'classnames';
+import ArrowIcon from '../../public/arrow.svg';
 
-export const Button = ({appearance, children, className, ...props}: ButtonProps): JSX.Element => {
+export const Button = ({appearance, children, arrow = 'none', className, ...props}: ButtonProps): JSX.Element => {
     return (
         <button className={cn(style.button, className, {
             [style.primary]: appearance === 'primary',
@@ -11,6 +12,11 @@ export const Button = ({appearance, children, className, ...props}: ButtonProps)
                 {...props}
         >
             {children}
+            {arrow !== 'none' && <span className={cn(style.arrow, {
+                [style.down]: arrow === 'down'
+            })}>
+                        <ArrowIcon/>
+            </span>}
         </button>
     );
 };
