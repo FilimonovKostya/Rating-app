841b5d8598f0dc3792b473eddaed5038046863c8
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-02-18 13:57:41 +0300

    set up Star component

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,10 +1,23 @@
 import {RatingProps} from "./Rating.props";
+import {useState} from "react";
+import style from './Rating.module.css';
 import StarIcon from './star.svg';
+import cn from 'classnames';
+
+const stars = new Array(5).fill(<></>);
 
 export const Rating = ({isEditable = false, setNumberRating, numberRating, className, ...props}: RatingProps) => {
+    const [rating, setRating] = useState(1);
+
     return (
         <div {...props}>
-            <StarIcon fill={'blue'}/>
+            {
+                stars.map((el, index) => <StarIcon className={cn(style.star, {
+                    [style.active]: index <= rating,
+                    [style.notActive]: index > rating,
+                })} key={index} onClick={() => setRating(index)}
+                />)
+            }
         </div>
     );
 };
